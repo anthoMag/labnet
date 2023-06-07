d164604bf027f47cfa711a9f5b1558d5cdc770e6
anthoMag <119311321+anthoMag@users.noreply.github.com>
2023-06-07 23:57:01 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/Header';
 import Home from './pages/home/Home';
-import Molecules from './components/ui/molecules/Molecules';
+import Card from './components/ui/molecules/card/Card';
 import './assets/css/scss/normalize.scss';
 import './assets/css/scss/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <Molecules></Molecules>
+      <Header></Header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/ui/molecules/card/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+
+    return (
+      <div className="card">
+        <p>test</p>
+      </div>
+    );
+  }
+
+export default Card;
