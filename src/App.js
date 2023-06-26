d5c222c6ac014dd5b016dfacf0cf81380740a86f
anthoMag <119311321+anthoMag@users.noreply.github.com>
2023-06-26 15:33:06 +0200

    delete route Card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './components/Header';
 import Home from './pages/home/Home';
-import Card from './components/ui/molecules/card/Card';
 import './assets/css/scss/normalize.scss';
 import './assets/css/scss/app.scss';
 
